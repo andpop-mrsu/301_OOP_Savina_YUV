12051bc3e8186615febe4ee5783965c59207e770
yuliya_dubrovskaya <julysept@ya.ru>
2025-03-27 15:56:36 +0300

    did Task05_1

---
+++ Task05/Task05_1/src/BooksList.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App;
+
+use Iterator;
+
+class BooksList implements Iterator
+{
+    private array $books = [];
+
+    public function add(Book $book): void
+    {
+        $this->books[] = $book;
+    }
+
+    public function count(): int
+    {
+        return count($this->books);
+    }
+
+    public function get(int $n): ?Book
+    {
+        return $this->books[$n] ?? null;
+    }
+
+    public function store(string $fileName): void
+    {
+        file_put_contents($fileName, serialize($this->books));
+    }
+
+    public function load(string $fileName): void
+    {
+        if (file_exists($fileName)) {
+            $data = file_get_contents($fileName);
+            $this->books = unserialize($data) ?: [];
+        }
+    }
+
+    public function rewind(): void
+    {
+        reset($this->books);
+    }
+
+    public function current(): Book
+    {
+        return current($this->books);
+    }
+
+    public function key(): int
+    {
+        return $this->current()->getId();
+    }
+
+    public function next(): void
+    {
+        next($this->books);
+    }
+
+    public function valid(): bool
+    {
+        return current($this->books) !== false;
+    }
+}
